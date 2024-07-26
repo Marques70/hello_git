d3f1d5a655bfb6146b5f9cea2f9e3bfbd0a53729
Marques70 <foraneosip@hotmail.es>
2024-07-26 02:11:18 +0200

    volvi a meterle otra linea mas

--- hellogit.py
+++ hellogit.py
@@ -1,4 +1,4 @@
 print("Hola clase. Estamos con git")
 print("Espero que esteis aprendiendo mucho")
 print("He añadido una tercera linea a este fichero Debe de quedar registrada")
-
+print("Voy a añadir una cuarta linea al ficherito")
